24fdf2f702753bff19c5528a2a950c2620972b73
Mahmud139 <mahmudulhasan139139@gmail.com>
2024-08-13 01:14:53 +0800

    updated

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,9 +15,17 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Fprintln(w, "create a new movie")
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
 
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }

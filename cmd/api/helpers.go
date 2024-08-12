416f868d503f54d7f66fb4f94ba2f81ead5865b2
Mahmud139 <mahmudulhasan139139@gmail.com>
2024-08-13 01:29:31 +0800

    updated :

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,7 +8,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) readIdParam(r *http.Request) (int, error) {
+func (app *application) readIdParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)

fb4fc8251fb580404841fa17b4a1d0b521a8ea41
Mahmud139 <mahmudulhasan139139@gmail.com>
2024-08-13 00:29:45 +0800

    updated main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -52,5 +52,7 @@ func main() {
 }
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintln(w, "environment: ", app.config.env)
+	fmt.Fprintln(w, version)
 }

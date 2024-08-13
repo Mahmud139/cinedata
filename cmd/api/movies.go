92a2c65b8ae17de6d3a12bb8c5f6c1199a7d5a0e
Mahmud139 <mahmudulhasan139139@gmail.com>
2024-08-13 21:02:41 +0800

    updated

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintln(w, "environment: ", app.config.env)
-	fmt.Fprintln(w, version)
+	data := map[string]string{
+		"status": "available",
+		"environment": app.config.env,
+		"version": version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounterd an error and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {

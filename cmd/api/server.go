3ab812393f69681495dce2da8da02310859774dd
Mahmud139 <mahmudulhasan139139@gmail.com>
2024-08-26 22:00:10 +0800

    added waitgroup

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -35,8 +35,18 @@ func (app *application) serve() error {
 
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
+		
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+		
+		app.logger.PrintInfo("completing background task", map[string]string{
+			"addr": srv.Addr,
+		})
 
-		shutdownError <- srv.Shutdown(ctx)
+		app.wg.Wait()
+		shutdownError <- nil
 	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{

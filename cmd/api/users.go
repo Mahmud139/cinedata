0f1b312abec6090b7b85e52645d35b5a2bc4f592
Mahmud139 <mahmudulhasan139139@gmail.com>
2024-08-28 18:18:30 +0800

    updated

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -80,7 +80,7 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 
 func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		TokenPlaintext string `json:"token_plaintext"`
+		TokenPlaintext string `json:"token"`
 	}
 
 	err := app.readJSON(w, r, &input)

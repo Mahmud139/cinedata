3f0ff4c22860f8f46feef6a2c2edc861a52012fe
Mahmud139 <mahmudulhasan139139@gmail.com>
2024-08-31 16:23:22 +0800

    updated

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,9 @@ type config struct {
 		password string
 		sender string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -65,6 +69,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "mahmudulhasan139139@gmail.com", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", os.Getenv("GOOGLE_APP_PASSWORD"), "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Cinedata <mahmudulhasan139139@gmail.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origin", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
